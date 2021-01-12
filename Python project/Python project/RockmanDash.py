f533ece4971cacbf6c66ae54c41ac532c702bec8
aksghwoddl <65700842+aksghwoddl@users.noreply.github.com>
2021-01-12 20:28:26 +0900

    first upload

---
+++ Python project/Python project/RockmanDash.py
@@ -0,0 +1,381 @@
+import pygame
+from pygame.locals import *
+import os
+import sys
+import math
+import random
+
+pygame.init()
+
+W, H = 800, 447
+win = pygame.display.set_mode((W,H))
+pygame.display.set_caption('록맨 대쉬')
+
+bg = pygame.image.load((os.path.join('images', 'bg.png')))
+bgX = 0
+bgX2 = bg.get_width()
+
+clock = pygame.time.Clock()
+
+class player(object):
+    run = [pygame.image.load(os.path.join('images', str(x) + '.png')) for x in range(8,16)]
+    jump = [pygame.image.load(os.path.join('images', str(x) + '.png')) for x in range(1,8)]
+    slide = [pygame.image.load(os.path.join('images', 'S1.png')),pygame.image.load(os.path.join('images', 'S2.png')),pygame.image.load(os.path.join('images', 'S2.png')),pygame.image.load(os.path.join('images', 'S2.png')), pygame.image.load(os.path.join('images', 'S2.png')),pygame.image.load(os.path.join('images', 'S2.png')), pygame.image.load(os.path.join('images', 'S2.png')), pygame.image.load(os.path.join('images', 'S2.png')), pygame.image.load(os.path.join('images', 'S3.png')), pygame.image.load(os.path.join('images', 'S4.png')), pygame.image.load(os.path.join('images', 'S5.png'))]
+    fall = pygame.image.load(os.path.join('images' , '0.png'))
+    jumpList = [1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,-1,-1,-1,-1,-1,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-4,-4,-4,-4,-4,-4,-4,-4,-4,-4,-4,-4]
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.jumping = False
+        self.sliding = False
+        self.slideCount = 0
+        self.jumpCount = 0
+        self.runCount = 0
+        self.slideUp = False
+        self.falling = False
+
+    def draw(self, win):
+        if self.falling:
+            win.blit(self.fall , (self.x , self.y +30))
+        elif self.jumping:
+            self.y -= self.jumpList[self.jumpCount] * 1.2
+            win.blit(self.jump[self.jumpCount//18], (self.x,self.y))
+            self.jumpCount += 1
+            if self.jumpCount > 108:
+                self.jumpCount = 0
+                self.jumping = False
+                self.runCount = 0
+            self.hitbox = (self.x+4 , self.y , self.width-24 , self.height-30)
+        elif self.sliding or self.slideUp:
+            if self.slideCount < 20:
+                self.y += 1
+            elif self.slideCount == 80:
+                self.y -= 19
+                self.sliding = False
+                self.slideUp = True
+            elif self.slideCount > 20 and self.slideCount < 80:
+                self.hitbox = (self.x , self.y+3 , self.width-8 , self.height-35)
+                
+            if self.slideCount >= 110:
+                self.slideCount = 0
+                self.slideUp = False
+                self.runCount = 0
+                self.hitbox = (self.x+4 , self.y , self.width-24 , self.height-10)
+            win.blit(self.slide[self.slideCount//10], (self.x,self.y))
+            self.slideCount += 1
+
+       
+            
+
+
+        else:
+            if self.runCount > 42:
+                self.runCount = 0
+            win.blit(self.run[self.runCount//6], (self.x,self.y))
+            self.runCount += 1
+            self.hitbox = (self.x+4 , self.y , self.width-23 , self.height-20)
+
+
+class saw(object):
+    rotate = [pygame.image.load(os.path.join('images', 'SAW0.PNG')),pygame.image.load(os.path.join('images', 'SAW1.PNG')),pygame.image.load(os.path.join('images', 'SAW2.PNG')),pygame.image.load(os.path.join('images', 'SAW3.PNG'))]
+    def __init__(self,x,y,width,height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.rotateCount = 0
+        self.vel = 1.4
+
+    def draw(self,win):
+        self.hitbox = (self.x + 10, self.y + 5, self.width - 20, self.height - 5)
+        if self.rotateCount >= 8:  ######적 움직이게 하기##############
+            self.rotateCount = 0
+        win.blit(pygame.transform.scale(self.rotate[self.rotateCount//2], (64,64)), (self.x,self.y))
+        self.rotateCount += 1
+
+    def collide(self , rect):
+        if rect[0] + rect[2] > self.hitbox[0] and rect[0] < self.hitbox[0] + self.hitbox[2]:
+            if rect[1] + rect[3] > self.hitbox[1]:
+                return True
+            return False
+        
+            
+class spike(saw):
+    img = pygame.image.load(os.path.join('images', 'spike.png'))
+    def draw(self,win):
+        self.hitbox = (self.x + 10, self.y , 28,315) 
+        win.blit(self.img, (self.x,self.y))
+
+        
+    def collide(self , rect):
+        if rect[0] + rect[2] > self.hitbox[0] and rect[0] < self.hitbox[0] + self.hitbox[2]:
+            if rect[1] < self.hitbox[3]:
+                return True
+            return False
+
+
+###########################################################변수 선언########################################################################
+run = True
+speed = 70
+
+pygame.time.set_timer(USEREVENT+1, 500)
+pygame.time.set_timer(USEREVENT+2, random.randrange(5000, 7000))
+objects = []
+pause = 0
+fallSpeed = 0
+score = 0
+
+
+def redrawWindow():
+    global score
+    win.blit(bg, (bgX, 0))  # 배경 1
+    win.blit(bg, (bgX2, 0))  # 배경 2
+    runner.draw(win)
+    for x in objects:
+       x.draw(win)
+    font = pygame.font.SysFont('test.ttf' , 30)
+    
+    text = font.render('Score : ' + str(score) , True , (255,255,255))
+    text2 = font.render('High Score: ' + str(updateFile()) , True , (255,255,255))
+    win.blit(text , (700,30))
+    win.blit(text2 , (650,10))
+    pygame.display.update()
+
+
+def updateFile():
+    f = open('scores.txt' , 'r')
+    file = f.readlines()
+    last = int(file[0])
+
+    if last < int(score):
+        f.close()
+        file = open('scores.txt' , 'w')
+        file.write(str(score))
+        file.close()
+
+        return score
+    return last
+
+pygame.mixer.music.load('./sound/bgm.mp3')
+pygame.mixer.music.play(-1 , 0.0)
+
+def startScreen():
+    
+
+    global run
+    global W , H
+    global clock
+
+    pygame.mixer.music.load('./sound/bgm.mp3')
+    pygame.mixer.music.play(-1 , 0.0)
+    
+    bg = pygame.image.load((os.path.join('images', 'StartBacks.png')))
+    keys = pygame.key.get_pressed()
+    pygame.init()
+    while True:
+        gamepad = pygame.display.set_mode((W,H))
+        BigFont = pygame.font.SysFont('test.ttf' , 120)
+        gamepad.blit(bg, (0,0))
+        Font = pygame.font.SysFont('test.ttf' , 80)
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                runGame()
+        
+
+
+            if event.type == pygame.QUIT:
+                run = False    
+                pygame.quit() 
+                quit()
+
+def endScreen1():
+    
+    global run
+    global W , H
+    global clock
+    global score
+    global objects
+    bg = pygame.image.load((os.path.join('images', 'EndBack1.png')))
+    keys = pygame.key.get_pressed()
+    pygame.mixer.music.stop()
+    pause = 0
+    pygame.init()
+    objects = []
+    
+    while True:
+        gamepad = pygame.display.set_mode((W,H))
+        gamepad.blit(bg, (0,0))
+        largeFont = pygame.font.SysFont('test.ttf' , 80)
+        previousScore = largeFont.render('High Score :' + str(updateFile()) , True , (255,255,255))
+        gamepad.blit(previousScore , (W/2 - previousScore.get_width()/2 ,50))
+        newScore = largeFont.render('Score :' + str(score) , True , (255,255,255))
+        gamepad.blit(newScore , (W/2 - newScore.get_width()/2 ,150))
+        pygame.display.update()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key==pygame.K_RETURN:
+                    run = True
+                    runGame()
+                if event.key==pygame.K_RIGHT:
+                    endScreen2()
+
+            if event.type == pygame.QUIT:
+                run = False    
+                pygame.quit() 
+                quit()
+    score = 0
+
+
+            
+def endScreen2():
+    global pause , objects , speed , score
+    gamepad = pygame.display.set_mode((W,H))
+    bg = pygame.image.load((os.path.join('images', 'EndBack2.png')))
+    pygame.mixer.music.stop()
+    pause = 0
+    font = pygame.font.SysFont('test.ttf' , 30)
+    objects = []
+    speed = 70
+    run = True
+    keys = pygame.key.get_pressed()
+    while run:
+        pygame.time.delay(100)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key==pygame.K_RETURN:
+                    pygame.quit()
+                    quit()
+                if event.key==pygame.K_LEFT:
+                    endScreen1()
+                
+
+
+
+            
+        gamepad.blit(bg , (0,0))
+        largeFont = pygame.font.SysFont('test.ttf' , 80)
+        previousScore = largeFont.render('High Score :' + str(updateFile()) , True , (255,255,255))
+        win.blit(previousScore , (W/2 - previousScore.get_width()/2 ,50))
+        newScore = largeFont.render('Score :' + str(score) , True , (255,255,255))
+        win.blit(newScore , (W/2 - newScore.get_width()/2 ,150))
+        pygame.display.update()
+    score = 0
+    
+
+def runGame():
+    global score
+    global W, H
+    global win
+
+    global bg
+    global bgX
+    global bgX2
+
+    global clock 
+    global run
+    global speed
+    global runner
+    pygame.time.set_timer(USEREVENT+1, 500)
+    pygame.time.set_timer(USEREVENT+2, random.randrange(5000, 7000))
+    pygame.time.set_timer(USEREVENT+3, random.randrange(3000, 5000))
+    global objects
+    global pause
+    global fallSpeed
+    global keys
+    
+    runner = player(100, 310, 64, 64)
+    pause = 0
+    fallSpeed = 0
+    score = 0
+    speed=70
+    runner.falling=False
+    pygame.mixer.music.play(-1 , 0.0)
+    
+    while run:
+        if(run):
+            score = speed//7 -10
+            if pause > 0:
+                pause += 1
+                if pause > fallSpeed *2:
+                    endScreen1()
+                    
+
+            for objectt in objects:
+                if objectt.collide(runner.hitbox):
+                    runner.falling = True
+
+                    if pause == 0:
+                        fallSpeed = speed
+                        pause = 1
+                        
+                objectt.x -= 1.4
+                if objectt.x < objectt.width * -1:
+                     objects.pop(objects.index(objectt))
+                 
+            
+            bgX -= 1.4
+            bgX2 -= 1.4
+
+            if bgX < bg.get_width() * -1:
+                bgX = bg.get_width()
+
+            if bgX2 < bg.get_width()* -1:
+                bgX2 = bg.get_width()
+                
+            for event in pygame.event.get():  
+                if event.type == pygame.QUIT: 
+                    run = False    
+                    pygame.quit() 
+                    quit()
+            
+                if event.type == USEREVENT+1:
+                    speed += 1
+                if event.type == USEREVENT+2:
+                    r = random.randrange(0,2)
+                    if r == 0:
+                        objects.append(saw(810, 310, 64, 64))
+                    elif r == 1:
+                        objects.append(spike(810, 0, 48, 310))
+                if score > 20 :
+                    event.type == USEREVENT+3
+                    if event.type == USEREVENT+3:
+                        r = random.randrange(0,2)
+                        if r == 0:
+                            objects.append(saw(810, 280, 64, 64))
+                        elif r == 1:
+                            objects.append(spike(810, 0, 48, 310))
+                        
+
+            clock.tick(speed) 
+
+            keys = pygame.key.get_pressed()
+
+            if keys[pygame.K_SPACE] or keys[pygame.K_UP]: ##############점프############
+                if not(runner.jumping): 
+                    runner.jumping = True
+
+            if keys[pygame.K_DOWN]:  ##########슬라이딩############
+                if not(runner.sliding):
+                    runner.sliding = True
+
+            if keys[pygame.K_r]:
+                pygame.mixer.music.stop()
+                pygame.mixer.music.play(-1 , 0.0)
+                startScreen()     
+
+            clock.tick(speed)
+            redrawWindow()
+
+
+#############################################메인함수###############################################################################
+
+if __name__ =='__main__':
+    startScreen()
+    
